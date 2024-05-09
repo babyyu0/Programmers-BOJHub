02fd7e720efcfda03173ec7bed63f5e47cd2c6e5
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-09 09:10:31 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ Python3/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -4,7 +4,7 @@ def solution(code):
     for i in range(0, len(code)):
         if code[i] == '1': #code[idx]가 1일 경우
             mode = (mode + 1) % 2
-        else:
-            answer += code[i] if mode + (i % 2) == 0 or mode + (i % 2) == 2 else ""
+        elif mode == i % 2:
+            answer += code[i]
             
     return "EMPTY" if len(answer) == 0 else answer
