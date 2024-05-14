ff5a43335a04cf4fe6858185f10d30d650392c38
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-14 12:53:29 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 3.33 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
+++ Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 여러 번 뒤집기 - 181913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181913?language=python3#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 5.31 ms
+메모리: 10.1 MB, 시간: 3.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 14일 12:50:52
+2024년 05월 14일 12:53:26
 
 ### 문제 설명
 
--- Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ Python3/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,7 +1,5 @@
 def solution(my_string, queries):
-    for query in queries:
-        # print("my_string 1:", my_string[:query[0]])
-        # print("my_string 2:", ''.join(reversed(my_string[query[0]:query[1] + 1])))
-        # print("my_string 3:", my_string[query[1]:])
-        my_string = my_string[:query[0]] + ''.join(reversed(my_string[query[0]:query[1] + 1])) + my_string[query[1] + 1:]
-    return my_string
+    answer = list(my_string)
+    for s,e in queries:
+        answer[s:e + 1] = answer[s:e + 1][::-1]
+    return ''.join(answer)
