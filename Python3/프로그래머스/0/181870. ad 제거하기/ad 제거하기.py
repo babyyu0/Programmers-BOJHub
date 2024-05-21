dd6ea8e23b74384242f57e99032d3a707024ae80
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-21 09:37:18 +0900

    [level 0] Title: ad 제거하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
+++ Python3/프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -1,5 +1,2 @@
 def solution(strArr):
-    answer = []
-    for strUnit in strArr:
-        if(strUnit.count("ad") == 0): answer.append(strUnit);
-    return answer
+    return [s for s in strArr if "ad" not in s]
