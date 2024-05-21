09a4a7efb807a19002ad441a08040e99c1a6babc
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-21 09:22:25 +0900

    [level 0] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- C++/프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/README.md
+++ C++/프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기 - 181872 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181872?language=cpp#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181872) 
 
 ### 성능 요약
 
-메모리: 3.68 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 21일 09:20:53
+2024년 05월 21일 09:22:20
 
 ### 문제 설명
 
--- C++/프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cpp
+++ C++/프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cpp
@@ -4,24 +4,5 @@
 using namespace std;
 
 string solution(string myString, string pat) {
-    vector<int> accSum(myString.size());
-    int lastIndex = 0;
-    
-    accSum[0] = (myString[0] == pat[0]) ? 1 : 0;
-    
-    for(int i = 1; i < myString.size(); i++) {
-        if (myString[i] == pat[accSum[i - 1]]) {
-            accSum[i] = accSum[i - 1] + 1;
-        } else if (myString[i] == pat[0]) {
-            accSum[i] = 1;
-        }
-        
-        if(accSum[i] == pat.size()) {
-            lastIndex = i;
-            accSum[i] %= pat.size();
-        }
-    }
-    
-    string answer(myString.begin(), myString.begin() + lastIndex + 1);
-    return answer;
+    return myString.substr(0, myString.rfind(pat) + pat.size());
 }
