00f1f9d7b2152f2e32dffa6ba712b9089b4e32e3
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-24 15:18:19 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ Python3/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -2,8 +2,6 @@ def solution(s):
     before = ' '
     answer = ""
     for c in s:
-        if before == ' ':
-            answer += c.upper()
-        else: answer += c.lower()
+        answer += c.upper() if before == ' ' else c.lower()
         before = c
     return answer
