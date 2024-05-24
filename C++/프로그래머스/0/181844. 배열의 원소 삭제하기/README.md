d95a79225296af36774073a06f23e44c9fc3e1b1
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-24 12:38:47 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- C++/프로그래머스/0/181844. 배열의 원소 삭제하기/README.md
+++ C++/프로그래머스/0/181844. 배열의 원소 삭제하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 원소 삭제하기 - 181844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181844?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181844) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 4.15 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 24일 12:36:53
+2024년 05월 24일 12:38:44
 
 ### 문제 설명
 
--- C++/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
+++ C++/프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cpp
@@ -1,17 +1,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<int> delete_list) {
     vector<int> answer;
-    int n = arr.size();
     for(int del : delete_list) {
         if(*find(arr.begin(), arr.end(), del) == del) {
-            remove(arr.begin(), arr.end(), del);
-            arr.resize(--n);
+            arr.erase(find(arr.begin(), arr.end(), del));
         }
     }
     return arr;
