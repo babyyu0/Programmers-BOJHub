8cdd9cdeb85b16f10998a89be6221aa257a211c9
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-24 14:36:07 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,14 @@
+def solution(bandage, health, attacks):
+    t, x, y = bandage
+    full = health
+    time, rest = 0, 0
+    for attt, attn in attacks:
+        rest = attt - time - 1
+        health = min(health + (x * rest), full)
+        health = min(health + int(rest / t) * y, full)
+        time = attt
+        health -= attn
+        # print("현재 시각", time, "에 몬스터에게", attn, "만큼 공격 당해 현재 체력", health)
+        if health <= 0: return -1
+        
+    return health
