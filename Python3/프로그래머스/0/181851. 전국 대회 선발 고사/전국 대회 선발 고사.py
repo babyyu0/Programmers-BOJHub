ba14eded58c136585c4fcd4d2871dcc3ebd1d15e
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-24 10:40:18 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ Python3/프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,4 +1,4 @@
 def solution(rank, attendance):
-    rank = [[101 if attendance[i] == False else rank[i], i] for i in range(len(rank))]
+    rank = [(x, i) for i, x in enumerate(rank) if attendance[i]]
     rank.sort()
     return 10000 * rank[0][1] + 100 * rank[1][1] + rank[2][1]
