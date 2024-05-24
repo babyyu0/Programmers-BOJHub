01c742e8a2569629e7c5becf70b7c8cabd89a39a
Zero <58788576+babyyu0@users.noreply.github.com>
2024-05-24 10:50:20 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

--- C++/프로그래머스/0/181850. 정수 부분/정수 부분.cpp
+++ C++/프로그래머스/0/181850. 정수 부분/정수 부분.cpp
@@ -1,10 +1,8 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int solution(double flo) {
-    int answer = floor(flo);
-    return answer;
+    return (int) flo;
 }
