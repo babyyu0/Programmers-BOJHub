40618fdc9398380a64276742284a371d81eb25c1
Zero <58788576+babyyu0@users.noreply.github.com>
2024-07-02 16:13:07 +0900

    [level 1] Title: 콜라 문제, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    while(n >= a) {
+        n = n - a + b;
+        answer += b;
+    }
+    return answer;
+}
