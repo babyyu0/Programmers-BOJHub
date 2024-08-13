16c6e38b4e393cd567c636c3340e823ed8b210ac
Zero <58788576+babyyu0@users.noreply.github.com>
2024-08-13 09:48:55 +0900

    [level 1] Title: 소수 찾기, Time: 2977.89 ms, Memory: 77.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ Python3/프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -3,15 +3,22 @@ import math
 
 def solution(n):
     answer = 0
+    nset = set()
     
     def is_prime(n):
-        nset = set()
-
         for i in range(2, int(math.sqrt(n) + 1)):
             if n % i == 0:
                 return False
-        return not nset
+        return True
     
     for i in range(2, n + 1):
-        answer += int(is_prime(i))
+        if i in nset: #소수 아님을 이미 알 경우
+            continue
+        elif is_prime(i): #소수일 때
+            answer += 1
+        
+        j = 2
+        while i * j <= n: #해당 값은 소수가 아님 (* n)
+            nset.add(i *j)
+            j += 1
     return answer
