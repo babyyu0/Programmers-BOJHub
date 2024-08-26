0f3737a17b3bcb561ba7aca8611c988db8f98a87
Zero <58788576+babyyu0@users.noreply.github.com>
2024-08-26 14:08:51 +0900

    [level 2] Title: 방문 길이, Time: 0.42 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ Python3/프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,5 +1,4 @@
 def solution(dirs):
-    answer = 0
     walk_set = set((0, 0, 0, 0))
     drc = {'U': [1, 0], 'D': [-1, 0], 'L': [0, -1], 'R': [0, 1]}
     answer = 0
@@ -9,10 +8,8 @@ def solution(dirs):
         dr, dc = drc[dir]
         nr, nc = r + dr, c + dc
         if abs(nr) <= 5 and abs(nc) <= 5:
-            if (r, c, nr, nc) not in walk_set and (nr, nc, r, c) not in walk_set:
-                answer += 1
-                walk_set.add((r, c, nr, nc))
+            walk_set.add((r, c, nr, nc))
+            walk_set.add((nr, nc, r, c))
             r, c = nr, nc
-            # print("(", nr, ",", nc, ")로 이동...")
     
-    return answer
+    return len(walk_set) // 2
