8d61cf6ea3ec0689fa236c3b95e3e1533bc7ff61
Zero <58788576+babyyu0@users.noreply.github.com>
2024-08-27 15:39:56 +0900

    [level 3] Title: 등굣길, Time: 0.06 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ C++/프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int dp[100][100] = {0};
+    dp[0][0] = 1;
+    
+    for(auto& puddle : puddles) {
+        dp[puddle[1] - 1][puddle[0] - 1] = -1;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(dp[i][j] == -1) {
+                continue;
+            }
+            if(0 <= i - 1 && dp[i - 1][j] != -1) {
+                dp[i][j] += dp[i - 1][j];
+            } if(0 <= j - 1 && dp[i][j - 1] != -1) {
+                dp[i][j] += dp[i][j - 1];
+            }
+            dp[i][j] %= 1000000007;
+        }
+    }
+    
+    return dp[n - 1][m - 1];
+}
