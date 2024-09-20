05b8e50d9d55586e34438a64bd91401bbf99d941
Zero <58788576+babyyu0@users.noreply.github.com>
2024-09-20 10:41:17 +0900

    [level 3] Title: 최고의 집합, Time: 1.63 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ Python3/프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -3,8 +3,7 @@ def solution(n, s):
     set_num = 0
     while s > 0:
         std = s // n #기준점이 되는 수
-        set_num = std if s >= std else s
-        answer.append(set_num)
-        s -= set_num
+        answer.append(std)
+        s -= std
         n -= 1
     return answer if min(answer) > 0 else [-1]
