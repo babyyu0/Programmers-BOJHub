96f9e8729a6411135bdcc9ca0e2fcce025f07be4
Zero <58788576+babyyu0@users.noreply.github.com>
2024-09-23 10:28:47 +0900

    [level 3] Title: 단속카메라, Time: 3.66 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ C++/프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    vector<vector<int>> cams;
+    bool flag;
+    sort(routes.begin(), routes.end(), [&](vector<int> a, vector<int> b)->bool {
+        return a[0] < b[0];
+    });
+    
+    for(auto& route : routes) {
+        flag = false;
+        for(int i = 0; i < cams.size(); i++) {
+            if(max(route[0], cams[i][0]) <= min(route[1], cams[i][1])) {
+                flag = true;
+                cams[i][0] = max(route[0], cams[i][0]);
+                cams[i][1] = min(route[1], cams[i][1]);
+            }
+        }
+        if(!flag) {
+            cams.push_back(route);
+        }
+    }
+    
+    
+    return cams.size();
+}
