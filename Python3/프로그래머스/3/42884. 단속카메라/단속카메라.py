4c8ff37fed45e5cd705c5a35ce1d6ba294c94f07
Zero <58788576+babyyu0@users.noreply.github.com>
2024-09-23 10:21:19 +0900

    [level 3] Title: 단속카메라, Time: 52.25 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ Python3/프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,15 @@
+def solution(routes):
+    cams, flag = [], True
+    
+    for route in sorted(routes, key=lambda x:x[0]):
+        flag = False
+        for i in range(len(cams)):
+            if max(route[0], cams[i][0]) <= min(route[1], cams[i][1]):
+                # print(route, "is in", cams[i])
+                flag = True
+                cams[i][0],cams[i][1] = max(route[0], cams[i][0]), min(route[1], cams[i][1])
+        if not flag:
+            cams.append(route)
+        # print("CAMS:", cams)
+    
+    return len(cams)
